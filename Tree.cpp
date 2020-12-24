b0f2a1843c6122c8250f365be8551e816ac85f4b
RaymondCvoz <sranchvook@outlook.com>
2020-12-24 14:18:01 +0800

    refresh

--- Tree.cpp
+++ Tree.cpp
@@ -1,35 +1,38 @@
 #include <bits/stdc++.h>
 
 const int lens = 1e4;
-int n,use[lens];
+int n, use[lens];
 
 class node
 {
-    public:
-    int deg,c,num;
-    std::vector <node*> v;
-    public:node(int num = 0,int c = 0)
+public:
+    int deg, c, num;
+    std::vector<node *> v;
+
+public:
+    node(int num = 0, int c = 0)
     {
-        this -> c = c;
-        this -> num = num;
+        this->c = c;
+        this->num = num;
     }
 };
 node *root;
-bool cmp(node *a,node *b)
+bool cmp(node *a, node *b)
 {
-    return a -> p < b -> p;
+    return a->p < b->p;
 }
-std::set <node*> tmp;
-
+std::set<node *> tmp;
 
 void calc(node *cnt)
 {
-    if(cnt -> v.empty())
+    if (cnt->v.empty())
     {
-        cnt -> deg = 1; return;
+        cnt->deg = 1;
+        return;
     }
-    for(auto i : cnt -> v) calc(i);
-    for(auto i : cnt -> v) cnt -> deg += i -> deg;
+    for (auto i : cnt->v)
+        calc(i);
+    for (auto i : cnt->v)
+        cnt->deg += i->deg;
     return;
-    
 }
